e9079ffaf91cbf079f7f1a7958b6655f26c5a34d
Assen Totin <assen.totin@gmail.com>
2017-01-08 15:53:51 +0200

    Fix Abount dialogue; add m4 directory with GTK2 macro

--- src/menu.c
+++ src/menu.c
@@ -29,26 +29,6 @@ void quitDialogClose(GtkWidget *widget, gpointer data) {
 
 
 void menu_cb_about (GtkAction *action, streamer_applet *applet) {
-/*
-	char msg1[1024];
-	sprintf(&msg1[0], "%s\n\nVersion: %s\n\n%s\n\n%s", _("MATE Streamer Applet"), VERSION, _("An applet which lets you listen to online radio streams."), _("Assen Totin <assen.totin@gmail.com>"));
-	GtkWidget *label = gtk_label_new (&msg1[0]);
-	applet->quitDialog = gtk_dialog_new_with_buttons (_("MATE Streamer Applet"), GTK_WINDOW(applet), GTK_DIALOG_MODAL, NULL);
-	GtkWidget *buttonOK = gtk_dialog_add_button (GTK_DIALOG(applet->quitDialog), GTK_STOCK_OK, GTK_RESPONSE_OK);
-	gtk_dialog_set_default_response (GTK_DIALOG (applet->quitDialog), GTK_RESPONSE_CANCEL);
-#ifdef HAVE_GTK2
-	gtk_container_add (GTK_CONTAINER (GTK_DIALOG(applet->quitDialog)->vbox), label);
-#elif HAVE_GTK3
-	gtk_container_add (GTK_CONTAINER(gtk_dialog_get_content_area(GTK_DIALOG(applet->quitDialog))), label);
-#endif
-
-	g_object_set(GTK_DIALOG (applet->quitDialog), "border-width", 10, NULL);
-
-	g_signal_connect (G_OBJECT(buttonOK), "clicked", G_CALLBACK (quitDialogClose), (gpointer) applet);
-
-	gtk_widget_show_all (GTK_WIDGET(applet->quitDialog));
-*/
-
 	GtkWidget *about = gtk_about_dialog_new();
 
 	gtk_about_dialog_set_program_name (GTK_ABOUT_DIALOG(about), _("MATE Streamer Applet"));
@@ -57,8 +37,10 @@ void menu_cb_about (GtkAction *action, streamer_applet *applet) {
 
 	gtk_about_dialog_set_copyright (GTK_ABOUT_DIALOG(about), "Copyleft 2013-1026. See License for details.");
 
-	const gchar *authors = "Assen Totin <assen.totin@gmail.com>";
-	gtk_about_dialog_set_authors (GTK_ABOUT_DIALOG(about),  &authors);
+	gchar[2] authors;
+	authors[0] = "Assen Totin <assen.totin@gmail.com>";
+	authors[1] = NULL;
+	gtk_about_dialog_set_authors (GTK_ABOUT_DIALOG(about),  &authors[0]);
 
 	gtk_about_dialog_set_translator_credits(GTK_ABOUT_DIALOG(about), _("translator-credits"));
 
